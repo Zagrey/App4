91cc77c46ff5aab458451b45e3182b5067a39d3f
Zagrey <zagrebin@gmail.com>
2016-12-21 00:53:58 +0300

    common fix

--- common/src/main/java/com/common/util/Greeting.java
+++ common/src/main/java/com/common/util/Greeting.java
@@ -1,8 +1,6 @@
 package com.common.util;
 
-import lombok.AllArgsConstructor;
 import lombok.Data;
-import lombok.NoArgsConstructor;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
@@ -10,8 +8,6 @@ import javax.persistence.Id;
 
 @Data
 @Entity
-@NoArgsConstructor
-@AllArgsConstructor
 public class Greeting {
     @Id
     @GeneratedValue
