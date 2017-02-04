a3381f96bc2afeb57f0c9f30d890fa1b7304df9a
Zagrey <zagrebin@gmail.com>
2017-02-04 23:54:35 +0300

    + Load/reload points +Points lifetime

--- app/src/main/java/com/android/zagrey/app4/MapsActivity.java
+++ app/src/main/java/com/android/zagrey/app4/MapsActivity.java
@@ -2,24 +2,42 @@ package com.android.zagrey.app4;
 
 import android.content.pm.PackageManager;
 import android.content.res.Resources;
+import android.location.Location;
 import android.os.AsyncTask;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.FragmentActivity;
 import android.util.Log;
 import com.common.util.Point;
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.location.LocationServices;
 import com.google.android.gms.maps.*;
-import com.google.android.gms.maps.model.CameraPosition;
-import com.google.android.gms.maps.model.LatLng;
-import com.google.android.gms.maps.model.MapStyleOptions;
-import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.gms.maps.model.*;
 import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
 import org.springframework.web.client.RestTemplate;
 
-public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
+import java.sql.Timestamp;
+import java.util.*;
+
+public class MapsActivity extends FragmentActivity implements GoogleMap.OnCameraMoveStartedListener,
+        GoogleMap.OnCameraMoveListener,
+        GoogleMap.OnMapClickListener,
+        GoogleMap.OnCameraMoveCanceledListener,
+        GoogleMap.OnCameraIdleListener,
+        OnMapReadyCallback,
+        GoogleApiClient.ConnectionCallbacks,
+        GoogleApiClient.OnConnectionFailedListener {
 
     private GoogleMap mMap;
+    private GoogleApiClient mGoogleApiClient;
     private static final String TAG = "MapsActivity";
+    private final String url = "http://192.168.1.197:8080/api/points";
+    private Location lastLocation;
+    private Timer mTimer;
+    private static final long TIMER_INTERVAL = 1000;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -31,25 +49,44 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         mapFragment.getMapAsync(this);
 
 
+        // Create an instance of GoogleAPIClient.
+        if (mGoogleApiClient == null) {
+            mGoogleApiClient = new GoogleApiClient.Builder(this)
+                    .addConnectionCallbacks(this)
+                    .addOnConnectionFailedListener(this)
+                    .addApi(LocationServices.API)
+                    .build();
+        }
     }
 
+    @Override
+    protected void onStart() {
+        super.onStart();
 
-    /**
-     * Manipulates the map once available.
-     * This callback is triggered when the map is ready to be used.
-     * This is where we can add markers or lines, add listeners or move the camera. In this case,
-     * we just add a marker near Sydney, Australia.
-     * If Google Play services is not installed on the device, the user will be prompted to install
-     * it inside the SupportMapFragment. This method will only be triggered once the user has
-     * installed Google Play services and returned to the app.
-     */
+        mGoogleApiClient.connect();
+        mTimer = new Timer();
+        mTimer.schedule(new TimerTask() {
+            @Override
+            public void run() {
+                timerMethod();
+            }
+
+        }, 0, TIMER_INTERVAL);
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+
+        mGoogleApiClient.disconnect();
+        mTimer.cancel();
+    }
+
     @Override
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
 
         try {
-            // Customise the styling of the base map using a JSON object defined
-            // in a raw resource file.
             boolean success = googleMap.setMapStyle(
                     MapStyleOptions.loadRawResourceStyle(
                             this, R.raw.mapstyle_aubergine));
@@ -61,35 +98,11 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
             Log.e(TAG, "Can't find style. Error: ", e);
         }
 
-        mMap.setOnMapClickListener(new GoogleMap.OnMapClickListener() {
-            @Override
-            public void onMapClick(LatLng latLng) {
-                Log.d(TAG, "onMapClick: " + latLng.latitude + ", " + latLng.longitude);
-
-                mMap.addMarker(new MarkerOptions().position(latLng).title("BOMB on: " + latLng.latitude + ",\n" + latLng.longitude));
-
-
-                RestTemplate restTemplate = new RestTemplate();
-                restTemplate.getMessageConverters().add(new MappingJackson2HttpMessageConverter());
-
-
-                Point p = Point.builder()
-                        .latitude(latLng.latitude)
-                        .longitude(latLng.longitude)
-                        .description("BOMB on: " + latLng.latitude + ", " + latLng.longitude)
-                        .build();
-
-
-                new HttpRequestTask().execute(p);
-            }
-        });
-
-        mMap.setOnCameraMoveListener(new GoogleMap.OnCameraMoveListener() {
-            @Override
-            public void onCameraMove() {
-
-            }
-        });
+        mMap.setOnCameraIdleListener(this);
+        mMap.setOnCameraMoveStartedListener(this);
+        mMap.setOnCameraMoveListener(this);
+        mMap.setOnCameraMoveCanceledListener(this);
+        mMap.setOnMapClickListener(this);
 
         if (ActivityCompat.checkSelfPermission(this,
                 android.Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED
@@ -119,18 +132,100 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
 //        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
     }
 
+    @Override
+    public void onMapClick(LatLng latLng) {
+        Log.d(TAG, "onMapClick: " + latLng.latitude + ", " + latLng.longitude);
+
+        mMap.addMarker(new MarkerOptions().position(latLng).title("BOMB on: " + latLng.latitude + ",\n" + latLng.longitude));
+
+
+        RestTemplate restTemplate = new RestTemplate();
+        restTemplate.getMessageConverters().add(new MappingJackson2HttpMessageConverter());
+
+
+        Point p = Point.builder()
+                .createdOn(new Timestamp(System.currentTimeMillis()))
+                .lifeTime(15)
+                .latitude(latLng.latitude)
+                .longitude(latLng.longitude)
+                .description("BOMB on: " + latLng.latitude + ", " + latLng.longitude)
+                .build();
+
+
+        new HttpRequestTask().execute(p);
+    }
+
+    @Override
+    public void onCameraMoveStarted(int reason) {
+
+        if (reason == GoogleMap.OnCameraMoveStartedListener.REASON_GESTURE) {
+            Log.d(TAG, "onCameraMoveStarted: The user gestured on the map (REASON_GESTURE)");
+        } else if (reason == GoogleMap.OnCameraMoveStartedListener
+                .REASON_API_ANIMATION) {
+            Log.d(TAG, "onCameraMoveStarted: The user tapped something on the map (REASON_API_ANIMATION)");
+        } else if (reason == GoogleMap.OnCameraMoveStartedListener
+                .REASON_DEVELOPER_ANIMATION) {
+            Log.d(TAG, "onCameraMoveStarted: The app moved the camera (REASON_DEVELOPER_ANIMATION)");
+        }
+    }
+
+    @Override
+    public void onCameraMove() {
+        Log.d(TAG, "onCameraMove: The camera is moving.");
+    }
+
+    @Override
+    public void onCameraMoveCanceled() {
+        Log.d(TAG, "onCameraMoveCanceled: Camera movement canceled.");
+    }
+
+    @SuppressWarnings("MissingPermission")
+    @Override
+    public void onCameraIdle() {
+        lastLocation = LocationServices.FusedLocationApi.getLastLocation(
+                mGoogleApiClient);
+
+        String location = lastLocation == null ?
+                "unknown" :
+                lastLocation.getLatitude() + ", " + lastLocation.getLongitude();
+        Log.d(TAG, "onCameraIdle: The camera has stopped moving. Last location: " + location);
+
+        try {
+
+            new PointsLoaderTask().execute();
+
+        } catch (Exception e) {
+            Log.e(TAG, e.getMessage());
+        }
+    }
+
+    @Override
+    public void onConnected(@Nullable Bundle bundle) {
+
+    }
+
+    @Override
+    public void onConnectionSuspended(int i) {
+
+    }
+
+    @Override
+    public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
+
+    }
+
     private class HttpRequestTask extends AsyncTask<Point, Void, Void> {
         @Override
         protected Void doInBackground(Point... params) {
             try {
-                final String url = "http://192.168.1.197:8080/api/points";
+
                 RestTemplate restTemplate = new RestTemplate();
                 restTemplate.getMessageConverters().add(new MappingJackson2HttpMessageConverter());
 
                 Point p = params[0];
                 restTemplate.postForObject(url, p, Point.class);
             } catch (Exception e) {
-                Log.e("MainActivity", e.getMessage(), e);
+                Log.e("MainActivity", e.getMessage());
             }
 
             return null;
@@ -145,4 +240,73 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
 //        }
 
     }
+
+    private class PointsLoaderTask extends AsyncTask<Void, Void, List<Point>> {
+        @Override
+        protected void onPostExecute(List<Point> points) {
+            super.onPostExecute(points);
+
+            Log.d(TAG, "onPostExecute: loaded points: " + points.size());
+
+            mMap.clear();
+
+            Timestamp timestamp = new Timestamp(System.currentTimeMillis());
+
+            for (Point p : points) {
+
+                if (timestamp.getTime() - (p.getCreatedOn().getTime() + 1000 * p.getLifeTime()) < 0) {
+                    mMap.addMarker(new MarkerOptions()
+                            .position(new LatLng(p.getLatitude(), p.getLongitude()))
+                            .title("BOMB on: " + p.getLatitude() + ",\n" + p.getLongitude()));
+                }
+            }
+        }
+
+        @Override
+        protected List<Point> doInBackground(Void... params) {
+            try {
+                Map<String, Double> par = new HashMap<>();
+                if (lastLocation != null) {
+                    par.put("latitude", lastLocation.getLatitude());
+                    par.put("longitude", lastLocation.getLongitude());
+                }
+
+                RestTemplate restTemplate = new RestTemplate();
+                restTemplate.getMessageConverters().add(new MappingJackson2HttpMessageConverter());
+
+                @SuppressWarnings({"unchecked", "InstantiatingObjectToGetClassObject"})
+                Point[] pointsArray = restTemplate.getForObject(url, new Point[0].getClass(), par);
+
+                //                Log.d(TAG, "PointsLoaderTask:doInBackground: loaded points: " + points);
+                return new ArrayList<>(Arrays.asList(pointsArray));
+            } catch (Exception e) {
+                Log.e(TAG, "PointsLoaderTask:doInBackground: ", e);
+            }
+            return null;
+        }
+    }
+
+    private void timerMethod()
+    {
+        //This method is called directly by the timer
+        //and runs in the same thread as the timer.
+
+        //We call the method that will work with the UI
+        //through the runOnUiThread method.
+//        this.runOnUiThread(Timer_Tick);
+
+        Log.d(TAG, "timerMethod: PointsLoaderTask().execute()");
+        new PointsLoaderTask().execute();
+    }
+
+
+//    private Runnable Timer_Tick = new Runnable() {
+//        public void run() {
+//
+//            //This method runs in the same thread as the UI.
+//
+//            //Do something to the UI thread here
+//
+//        }
+//    };
 }
--- common/src/main/java/com/common/util/Point.java
+++ common/src/main/java/com/common/util/Point.java
@@ -5,9 +5,11 @@ import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
+import java.sql.Timestamp;
 
 @Data
 @Entity(name = "points")
@@ -23,4 +25,8 @@ public class Point {
     private String description;
     private Double latitude;
     private Double longitude;
+    @Column(columnDefinition="TIMESTAMP WITHOUT TIME ZONE NOT NULL DEFAULT now()")
+    private Timestamp createdOn;
+    @Column(columnDefinition="INTEGER DEFAULT 15")
+    private Integer lifeTime;
 }
